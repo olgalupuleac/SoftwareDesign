011e2154b79e61850ab2bd6a43875b3e74a8cbf0
Olga Lupuleac <olga.lupuleac@gmail.com>
2019-02-08 18:39:25 +0300

    some comments

--- hw1/src/main/scala/ru/spbau/lupuleac/cli/Argument.scala
+++ hw1/src/main/scala/ru/spbau/lupuleac/cli/Argument.scala
@@ -2,12 +2,27 @@ package ru.spbau.lupuleac.cli
 
 import scala.io.Source
 
+/**
+  * Can be passed to the command
+  */
 trait Argument {
+  /*
+  The argument is interpreted as file.
+  Returns string representation of the argument.
+   */
   def asFile: String
 
+  /*
+  The argument is interpreted as file.
+  Returns file contents
+   */
   def asString: String
 }
 
+/**
+  * Argument from the command line.
+  * @param text is an argument itself
+  */
 case class CommandLineArgument(text: String) extends Argument {
   override def asFile: String = {
     val file = Source.fromFile(text)
@@ -19,18 +34,14 @@ case class CommandLineArgument(text: String) extends Argument {
   override def asString: String = text
 }
 
-
-object CommandLineArgument {
-  def apply(text: String): CommandLineArgument = new CommandLineArgument(text)
-}
-
-class Output(text: String) extends Argument {
+/**
+  * A output of the command.
+  * It is passed to the next command if the command was followed by pipe.
+  * @param text is the text which is returned by the command
+  */
+case class Output(text: String) extends Argument {
   override def asFile: String = text
 
+  // > echo x | echo -- empty output
   override def asString: String = ""
 }
-
-object Output {
-  def apply(text: String): Output = new Output(text)
-}
-
