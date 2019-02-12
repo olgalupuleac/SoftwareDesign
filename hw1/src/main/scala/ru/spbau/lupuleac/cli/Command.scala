5cf8ff517b3e3ec6e397a793cb1d9259efa582f6
Olga Lupuleac <olga.lupuleac@gmail.com>
2019-02-12 23:40:39 +0300

    implemented grep

--- hw1/src/main/scala/ru/spbau/lupuleac/cli/Command.scala
+++ hw1/src/main/scala/ru/spbau/lupuleac/cli/Command.scala
@@ -1,8 +1,11 @@
 package ru.spbau.lupuleac.cli
 
+import org.rogach.scallop._
+
+import scala.collection.mutable
 import scala.io.Source
 import scala.sys.process._
-import org.rogach.scallop._
+import scala.util.matching.Regex
 
 /**
   * A output of the command.
@@ -116,7 +119,7 @@ case class GrepConf(arguments: Seq[String]) extends ScallopConf(arguments) {
   val ignoreCase: ScallopOption[Boolean] = toggle(name = "ignore-case", default = Some(false), descrYes = "Ignore case distinctions in both the PATTERN and the input files.")
   val wordRegex: ScallopOption[Boolean] = toggle(name = "word-regexp", default = Some(false), descrYes = "Select only those lines containing matches that form whole words.\n" +
     "The test is that the matching substring must either be at the beginning of the line, or preceded by a non-word constituent character. Similarly, it must be either at the end of the line or followed by a non-word constituent character. Word-constituent characters are letters, digits, and the underscore.")
-  val afterContext: ScallopOption[Int] = opt[Int](short = 'A', descr = "Print NUM lines of trailing context after matching lines.", validate = x => x >= 0, argName = "NUM")
+  val afterContext: ScallopOption[Int] = opt[Int](short = 'A', descr = "Print NUM lines of trailing context after matching lines.", validate = x => x >= 0, argName = "NUM", default = Some(0))
   val pattern: ScallopOption[String] = trailArg[String](validate = x => try {
     x.r
     true
@@ -129,12 +132,50 @@ case class GrepConf(arguments: Seq[String]) extends ScallopConf(arguments) {
 
 case class GrepCommand(output: Output) extends Command {
 
+  def matchWord(pattern: Regex, line: String): Boolean = {
+    val mi = pattern.findAllIn(line)
+    while (mi.hasNext) {
+      val m = mi.next
+      if (m matches "[a-z|A-Z|0-9|_]*") {
+        return true
+      }
+    }
+    false
+  }
+
+  def lineToPrint(filename: String, line: String): String = if (filename == "") line else filename + ": " + line
+
   override def apply(arguments: String*): Output = {
     val conf = GrepConf(arguments)
-    if (conf.files.isEmpty) {
 
+    val files = if (conf.files.isEmpty) {
+      List(("", output.text))
+    } else {
+      conf.files().map(x => (x, File(x)))
     }
-    output
+    val pattern = conf.pattern().r
+    val res = mutable.MutableList[String]()
+    for ((filename, contents) <- files) {
+      var num = 0
+      for (line <- contents.split("\n")) {
+        val lineToMatch = if (conf.ignoreCase()) line.toLowerCase else line
+        val hasMatch = if (conf.wordRegex()) {
+          matchWord(pattern, lineToMatch)
+        } else {
+          pattern.findFirstIn(lineToMatch).isDefined
+        }
+        if (hasMatch) {
+          res += lineToPrint(filename, line)
+          num = conf.afterContext()
+        } else {
+          if (num > 0) {
+            res += lineToPrint(filename, line)
+            num -= 1
+          }
+        }
+      }
+    }
+    Output(res.mkString("\n"))
   }
 }
 
